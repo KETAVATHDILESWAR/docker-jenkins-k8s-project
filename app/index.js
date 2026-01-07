e703b5077e1265c8fde39f8b2c764ad342bd7136
KETAVATHDILESWAR <dileswarketavath2002@gmail.com>
2026-01-07 12:49:33 +0530

    Add basic HTTP server in index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>🚀 CI/CD Pipeline with Docker, Jenkins & Kubernetes</h1>');
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
